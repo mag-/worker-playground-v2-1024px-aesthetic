a3249e20c1381af4fe7431b076f70b02073fdcba
^^MAg^^ <mag@entropy.be>
2023-12-06 04:06:26 +0000

    return pil image, not numpy array

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -109,7 +109,6 @@ def generate_image(job):
         width=job_input['width'],
         num_inference_steps=job_input['num_inference_steps'],
         guidance_scale=job_input['guidance_scale'],
-        output_type="latent",
         num_images_per_prompt=job_input['num_images'],
         generator=generator
     ).images
